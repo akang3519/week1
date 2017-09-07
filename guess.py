a9fe8ab87dd3d5e22adaff2a8d512d1807ab682e
akang3519 <akang3519@gmail.com>
2017-09-07 09:41:49 -0400

    guess program

---
+++ guess.py
@@ -0,0 +1,10 @@
+secret = 3;
+
+for i in range(0,3):
+    guess = int(input("Guess a number: "))
+    if guess == secret:
+        print("You guessed correctly!")
+        break;
+    elif guess != secret:
+        print("Incorrect Guess")
+        break;
